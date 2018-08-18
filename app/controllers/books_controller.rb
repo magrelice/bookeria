e340cf0adda403ce9495561b3eaa571bac2a58b5
magrelice <fernanda.pilat@creditas.com.br>
2018-08-18 15:48:27 -0300

    Criando resources de Book

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,31 @@
+class BooksController < ApplicationController
+	# GET 
+	def index
+		@book = Book.all #lista de todos os livros do banco de dados 
+	end
+
+	# GET 
+	def new 
+		@book = Book.new #criando um objeto do tipo book
+	end	
+
+	# POST	
+	def create 
+		@book = Book.new(books_params) # crio um objeto com os dados que vieram do usuário 
+		@book.save # salvo no banco de dados e ele vai criar um ID no banco de dados 
+
+		#
+		redirect_to @book
+	end
+
+	# GET 
+	def show 
+	  @book = Book.find(params[:id])
+	end
+	  
+	private 
+
+	def books_params 
+		params.require( :book).permit(:title, :author, :genre, :year)
+	end	
+end
